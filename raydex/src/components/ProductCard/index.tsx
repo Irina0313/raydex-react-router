bbd8d49e52fd56e867353f14634e9baa18412289
Iryna Kanavalchuk <irkinbk@gmail.com>
2024-01-30 01:11:21 +0300

    catalog develop

--- raydex/src/components/ProductCard/index.tsx
+++ raydex/src/components/ProductCard/index.tsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
 import { Card } from 'antd';
-import ImagesCarousel from '../ImagesCarusel';
+import ImagesCarousel from '../ImagesCarousel';
 
 import CustomButton from '../button';
 import { ProductItemType } from '../../lib/catalog/products/01_XRayAndGammaRadiationDosimeters/ElectronicDosimeters';
+
 import { findPathByName } from '../../utils/getFullPath';
 
 const { Meta } = Card;
@@ -19,41 +20,42 @@ const ProductCard: React.FC<ProductCardProps> = ({
 }) => {
   return (
     <>
-      {category.map(
-        (p) =>
-          p && (
-            <Card
-              key={p?.id}
-              hoverable
-              style={{
-                width: 400,
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
-              }}
-              cover={
-                <ImagesCarousel
-                  images={p.images}
-                  imagePathPrefix={imagePathPrefix}
-                />
-              }
-            >
-              <Meta
-                title={
-                  <Link
-                    style={{ overflow: 'visible' }}
-                    to={`${findPathByName(p.name)}`}
-                    /* onClick={() => handleClick(p.name)} */
-                  >
-                    {p.name}
-                  </Link>
+      {category &&
+        category.map(
+          (p) =>
+            p && (
+              <Card
+                key={p?.id}
+                hoverable
+                style={{
+                  width: 400,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'space-between',
+                }}
+                cover={
+                  <ImagesCarousel
+                    images={p.images}
+                    imagePathPrefix={imagePathPrefix}
+                  />
                 }
-                description={`${p.status}`}
-              />
-              <CustomButton text="Заказать" />
-            </Card>
-          ),
-      )}
+              >
+                <Meta
+                  title={
+                    <Link
+                      style={{ overflow: 'visible' }}
+                      to={`${findPathByName(p.name)}`}
+                      /* onClick={() => handleClick(p.name)} */
+                    >
+                      {p.name}
+                    </Link>
+                  }
+                  description={`${p.status}`}
+                />
+                <CustomButton text='Заказать' />
+              </Card>
+            )
+        )}
     </>
   );
 };
